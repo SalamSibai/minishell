217165d3adafc9e2cdcbbdf304341de89b4bdb1c
Mohammed Mohammed <mohammoh@lab3r8s11.42abudhabi.ae>
2024-05-29 21:15:12 +0400

    checked

--- parsing/set_tokens.c
+++ parsing/set_tokens.c
@@ -35,13 +35,13 @@ void	set_type(t_token *token)
 		token->type = REDIRECT_APPEND;
 		printf("redirect append\n");
 	}
-	else if (ft_strcmp(token->token_string[0], '\"'))
+	else if (ft_strcmp(token->token_string[0], "\"") == 0)
 	{
 		//loop until we find the next "
 		token->type = DQOUTES;
 		printf("string in double qoutes");
 	}
-	else if (ft_strncmp(token->token_string[0], '\''))
+	else if (ft_strcmp(token->token_string[0], "\'") == 0)
 	{
 		//loop until we find the next "
 		token->type = SQOUTES;
